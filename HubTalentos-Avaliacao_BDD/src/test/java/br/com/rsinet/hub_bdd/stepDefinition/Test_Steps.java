9c0842061f28148b47a455e8addc6dbb090f5383
alex.junior <alex.junior@RSI1521.rsinet.com.br>
2020-01-21 09:51:52 -0300

    Classe implementada para utilização do Cucumber Api.

---
+++ HubTalentos-Avaliacao_BDD/src/test/java/br/com/rsinet/hub_bdd/stepDefinition/Test_Steps.java
@@ -0,0 +1,49 @@
+package br.com.rsinet.hub_bdd.stepDefinition;
+
+import cucumber.api.PendingException;
+import cucumber.api.java.pt.Dado;
+import cucumber.api.java.pt.Então;
+import cucumber.api.java.pt.Quando;
+
+public class Test_Steps {
+	
+	
+	@Dado("^que eu estou na home do site$")
+	public void que_eu_estou_na_home_do_site() throws Throwable {
+	    // Write code here that turns the phrase above into concrete actions
+	    throw new PendingException();
+	}
+
+	@Quando("^clico no icone de login$")
+	public void clico_no_icone_de_login() throws Throwable {
+	    // Write code here that turns the phrase above into concrete actions
+	    throw new PendingException();
+	}
+
+	@Quando("^clico em criar nova conta$")
+	public void clico_em_criar_nova_conta() throws Throwable {
+	    // Write code here that turns the phrase above into concrete actions
+	    throw new PendingException();
+	}
+
+	@Quando("^preencho todos os dados$")
+	public void preencho_todos_os_dados() throws Throwable {
+	    // Write code here that turns the phrase above into concrete actions
+	    throw new PendingException();
+	}
+
+	@Quando("^clico no botão registar$")
+	public void clico_no_botão_registar() throws Throwable {
+	    // Write code here that turns the phrase above into concrete actions
+	    throw new PendingException();
+	}
+
+	@Então("^eu consigo criar o meu usuário$")
+	public void eu_consigo_criar_o_meu_usuário() throws Throwable {
+	    // Write code here that turns the phrase above into concrete actions
+	    throw new PendingException();
+	}
+	
+	
+
+}
