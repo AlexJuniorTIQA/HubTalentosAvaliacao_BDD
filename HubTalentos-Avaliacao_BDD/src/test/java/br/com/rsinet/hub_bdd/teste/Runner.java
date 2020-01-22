bc9d708904509673d6b83e6bf62b62942f042c25
alex.junior <alex.junior@RSI1521.rsinet.com.br>
2020-01-22 10:00:44 -0300

    Classe que ação do cucumber com seus respectivos parametros.

---
+++ HubTalentos-Avaliacao_BDD/src/test/java/br/com/rsinet/hub_bdd/teste/Runner.java
@@ -0,0 +1,11 @@
+package br.com.rsinet.hub_bdd.teste;
+
+import org.junit.runner.RunWith;
+
+import cucumber.api.CucumberOptions;
+import cucumber.api.junit.Cucumber;
+
+@RunWith(Cucumber.class)
+@CucumberOptions(features = "feature")
+public class Runner {
+}
