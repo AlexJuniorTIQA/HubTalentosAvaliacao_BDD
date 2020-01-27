491dd0664ba51d1265bb6fb2b90f75af2e5475a4
alex.junior <alex.junior@RSI1521.rsinet.com.br>
2020-01-27 15:26:51 -0300

    Incluido o report na classe.

--- HubTalentos-Avaliacao_BDD/src/test/java/br/com/rsinet/hub_bdd/runner/RunnerTest.java
+++ HubTalentos-Avaliacao_BDD/src/test/java/br/com/rsinet/hub_bdd/runner/RunnerTest.java
@@ -1,16 +1,25 @@
 package br.com.rsinet.hub_bdd.runner;
 
+import java.io.File;
+import java.io.IOException;
+
+import org.junit.AfterClass;
 import org.junit.runner.RunWith;
+
+import com.cucumber.listener.Reporter;
 
 import cucumber.api.CucumberOptions;
 import cucumber.api.junit.Cucumber;
 
 @RunWith(Cucumber.class)
-@CucumberOptions(features = "./feature/ConsultaProdutoBarraDePesquisa.feature",
+@CucumberOptions(features = "./feature/",
 				glue = "br.com.rsinet.hub_bdd.stepDefinition",
-				monochrome = true)
+				plugin = {
+						"com.cucumber.listener.ExtentCucumberFormatter:target/reports/StepsReportTest.html" })
 public class RunnerTest {
-
+	@AfterClass
+	public static void writeExtentReport() throws IOException { 		
+		Reporter.loadXMLConfig(new File("./extent-config.xml"));
+	}
 }
-
 
