594c82eed64a8c8d37a68a2ccc3e935d16e0a84f
alex.junior <alex.junior@RSI1521.rsinet.com.br>
2020-01-27 15:27:09 -0300

    Sem alterações válidas.

--- HubTalentos-Avaliacao_BDD/src/test/java/br/com/rsinet/hub_bdd/teste/ConsultaProdutoTelaInicial.java
+++ HubTalentos-Avaliacao_BDD/src/test/java/br/com/rsinet/hub_bdd/teste/ConsultaProdutoTelaInicial.java
@@ -7,6 +7,10 @@ import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.support.PageFactory;
 
+import com.aventstack.extentreports.ExtentReports;
+import com.aventstack.extentreports.ExtentTest;
+import com.aventstack.extentreports.reporter.ExtentHtmlReporter;
+
 import br.com.rsinet.hub_bdd.files.Constant;
 import br.com.rsinet.hub_bdd.files.ExcelUtils;
 import br.com.rsinet.hub_bdd.files.Screenshot;
@@ -14,43 +18,39 @@ import br.com.rsinet.hub_bdd.page.DriverElement;
 import br.com.rsinet.hub_bdd.page.HomePage;
 import br.com.rsinet.hub_bdd.page.ProductPage;
 
-
 public class ConsultaProdutoTelaInicial {
 
 	private WebDriver driver;
-	
 
 	@Before
 	public void Inicializa() throws Exception {
-		
+
 		driver = DriverElement.getChromeDriver();
 	}
-	
+
 	@After
 	public void finaliza() {
 		DriverElement.quitDriver(driver);
 	}
+
 	@Test
-	public void ConsultarProdutoSucesso ()throws Exception  {
+	public void ConsultarProdutoSucesso() throws Exception {
 		HomePage homePage = PageFactory.initElements(driver, HomePage.class);
 		ProductPage productPage = PageFactory.initElements(driver, ProductPage.class);
-		ExcelUtils.setExcelFile(Constant.File_DataUserRegister,"Mice");
-		
+		ExcelUtils.setExcelFile(Constant.File_DataUserRegister, "Mice");
+
 		homePage.javaScriptClick(By.id("miceTxt"));
 		homePage.findElementLinkText(ExcelUtils.getCellData(3, 1));
-		
+
 		productPage.assertEqualsProduct(ExcelUtils.getCellData(3, 1));
 		Screenshot.getScreenShot(driver, "TesteConsultaTelaPrincipalSucesso ");
 	}
-	
+
 	@Test
-	public void ConsultarProdutoFalha ()throws Exception  {
+	public void ConsultarProdutoFalha() throws Exception {
 		HomePage homePage = PageFactory.initElements(driver, HomePage.class);
-		
-		
 		homePage.ClickEelementHPEliteBookFolioDetails();
-	
 
 	}
-	
+
 }
